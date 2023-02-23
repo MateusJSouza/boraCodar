e520e4836e0dab3e79a0e60344b09f2e66a297c1
Mateus Jesus <mateusjdev@gmail.com>
2023-02-22 23:04:08 -0300

    feat: add funcionalidade de pesquisa por nome

---
+++ 07/script.js
@@ -0,0 +1,47 @@
+const filterElementsByName = document.querySelector('form input');
+const cards = document.querySelectorAll('.cards .card');
+
+filterElementsByName.addEventListener('input', filterCardsByName);
+
+function filterCardsByName() {
+  if (filterElementsByName.value != '') {
+    for (let card of cards) {
+      let title = card.querySelector('h3');
+      title = title.textContent.toLowerCase();
+
+      let filterText = filterElementsByName.value.toLowerCase();
+
+      console.log(title);
+
+      if (!title.includes(filterText)) {
+        card.style.display = 'none';
+      } else {
+        card.style.display = 'block';
+      }
+    }
+  } else {
+    for (let card of cards) {
+      card.style.display = 'block';
+    }
+  }
+}
+
+const selectCity = document.getElementById('city');
+
+selectCity.addEventListener('change', filterCardsByCity);
+
+function filterCardsByCity() {
+  const selectedCity = selectCity.value;
+  const cityCard = document.querySelector(`#${selectedCity}`);
+  const elements = document.querySelectorAll('.card-city');
+  // const ifItsMoreThanOne = document.querySelectorAll('isMoreThanOne');
+
+  for (const element of elements) {
+    if (element === cityCard) {
+      element.style.display = 'block';
+    } else {
+      element.style.display = 'none';
+    }
+  }
+}
+
